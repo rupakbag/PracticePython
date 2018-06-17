d4bb723f7bf331b2525b622b7cc50885779469c8
Rupak Bag <rupak.bag@gmail.com>
2018-06-17 13:26:01 -0400

    Added till Ex 7

--- src/pp1-4.py
+++ src/pp1-4.py
@@ -16,10 +16,26 @@
 # print([x for x in a if x < 5])
 
 # Ex 4
-try:
-    num = int(input("Enter a number "))
-except ValueError:
-    print("Invalid input")
-    exit()
-l = range(1, num)
-print([i for i in l if num%i == 0])
+# try:
+#     num = int(input("Enter a number "))
+# except ValueError:
+#     print("Invalid input")
+#     exit()
+# l = range(1, num)
+# print([i for i in l if num%i == 0])
+
+# Ex 5
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+# print([x for x in a if x in b])
+
+# Ex 6
+# s = input("Enter a string ")
+# i = len(s)//2
+# print ("Palindrome" if s[0:i:1] == s[:i-1:-1] else "Not Palindrome")
+
+# Ex 7
+# a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+# print([x for x in a if x%2==0])
+
+# Ex 8
